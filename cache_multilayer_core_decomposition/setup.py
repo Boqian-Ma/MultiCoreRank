b7effcaa5723d8c8a70dd89939df17dbb39fede9
Boqian Ma <35411147+Boqian-Ma@users.noreply.github.com>
2021-11-01 23:41:59 +1100

    remove backup

--- cache_multilayer_core_decomposition/setup.py
+++ cache_multilayer_core_decomposition/setup.py
@@ -5,17 +5,11 @@ from shutil import copyfile
 if __name__ == '__main__':
     # files to be included in the extensions
     paths = [
-        'core_decomposition/densest_subgraph/core',
-        'core_decomposition/densest_subgraph/densest_subgraph',
-        'core_decomposition/densest_subgraph/objective_function',
         'core_decomposition/subroutines/commons',
         'core_decomposition/subroutines/core',
         'core_decomposition/subroutines/core_decomposition',
         'core_decomposition/subroutines/pure_core_decomposition',
         'core_decomposition/breadth_first',
-        'core_decomposition/depth_first',
-        'core_decomposition/hybrid',
-        'core_decomposition/naive',
         'multilayer_graph/multilayer_graph',
         'scripts/filter_distinct_cores',
         'utilities/memory_measure',
--- multilayer_core_decomposition/core_decomposition/test.py
+++
@@ -1,7 +0,0 @@
-
-dict_a = {"a":15}
-
-dict_b = {"a": 16}
-
-
-print(dict_a)
