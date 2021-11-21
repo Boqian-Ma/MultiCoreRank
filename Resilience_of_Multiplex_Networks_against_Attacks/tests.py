148d3a267a3576a638c1d8cf9e829eb395f78c76
Boqian Ma <35411147+Boqian-Ma@users.noreply.github.com>
2021-11-21 23:48:19 +1100

    more tests

--- Resilience_of_Multiplex_Networks_against_Attacks/tests.py
+++ Resilience_of_Multiplex_Networks_against_Attacks/tests.py
@@ -1,23 +1,72 @@
 from multilayer_graph.multilayer_graph import MultilayerGraph
+import numpy as np
+import pytest
 
+DATA_SET = "example"
 
-def test_remove_node(dataset):
-    multilayer_graph = MultilayerGraph(dataset)
+#############################
+# multilayer_graph.py tests #
+#############################
+def test_remove_node():
+    multilayer_graph = MultilayerGraph(DATA_SET)
+
     multilayer_graph.remove_node(1)
     multilayer_graph.remove_node(2)
-    print(multilayer_graph.adjacency_list)
 
+def test_get_connected_nodes():
+    pass
 
-def test_get_nodes(dataset):
-    multilayer_graph = MultilayerGraph(dataset)
-    assert(multilayer_graph.get_connected_nodes() == set([1, 2, 3, 4, 5, 6]))
+def test_get_nodes():
+    multilayer_graph = MultilayerGraph(DATA_SET)
+    assert(multilayer_graph.get_nodes() == set([1, 2, 3, 4, 5, 6]))
+
     multilayer_graph.remove_node(1)
-    assert(multilayer_graph.get_connected_nodes() == set([2, 3, 4, 5, 6]))
+    assert(multilayer_graph.get_nodes() == set([2, 3, 4, 5, 6]))
+
+    multilayer_graph.remove_node(2)
+    assert(multilayer_graph.get_nodes() == set([3, 4, 5, 6]))
+
+    multilayer_graph.remove_node(3)
+    assert(multilayer_graph.get_nodes() == set([4, 5, 6]))
+
+    multilayer_graph.remove_node(4)
+    assert(multilayer_graph.get_nodes() == set([5, 6]))
+
+    multilayer_graph.remove_node(5)
+    assert(multilayer_graph.get_nodes() == set([]))
+
+    multilayer_graph.remove_node(6)
+
+    assert(multilayer_graph.get_nodes() == set([]))
+
+def test_remove_nodes():
+    multilayer_graph = MultilayerGraph(DATA_SET)
+
+    assert(multilayer_graph.get_nodes() == set([1, 2, 3, 4, 5, 6]))
+
+    multilayer_graph.remove_nodes([1, 2])
+    assert(multilayer_graph.get_nodes() == set([3, 4, 5, 6]))
+
+    multilayer_graph.remove_nodes([3, 4, 5])
+    assert(multilayer_graph.get_nodes() == set([]))
+
+def test_get_number_of_edges():
+    multilayer_graph = MultilayerGraph(DATA_SET)
+
+    assert(multilayer_graph.get_number_of_edges() == 17)
+
+    multilayer_graph.remove_node(1)
+    assert(multilayer_graph.get_number_of_edges() == 13)
+
+    multilayer_graph.remove_nodes([2, 3, 4, 5, 6])
+    assert(multilayer_graph.get_number_of_edges() == 0)
 
 
 
+def test_pearson_correlation_coefficient():
 
-#test_remove_node("example")
+    multilayer_graph = MultilayerGraph("europe")
 
-test_get_nodes("example")
+    print(multilayer_graph.pearson_correlation_coefficient())
 
+test_pearson_correlation_coefficient()
